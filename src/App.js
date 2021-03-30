1c1942dc7a84292aee069e26f29637621493d9f0
Pritish0173 <pnair.pritish0173@gmail.com>
2021-03-30 22:46:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="block">
+      <p>Connect four is a vertical game of tic-tac-toe, but with a twist-you have to get four in a row. 
+        The first player to get four checkers lined up in a row in any direction-horizontal, vertical, 
+        or diagonal-wins the game</p>
+      <p>The standard game size for a Connect 4 game is a 6x7 grid consisting of 42 cells.</p>
+      <p>Each player takes turns dropping a token into a column — which drops to the lowest available slot. 
+        Once settled, the players check to see if their newly placed piece. 
+        in combination with any existing pieces of their same color. 
+        make a straight light of four tokens. 
+        If four tokens are connected in a horizontal/vertical/diagonal line, that player wins.</p>
+      <p>A valid connection must have four tokens that are immediately adjacent to each other, 
+        and they can be vertical, diagonal, or horizontal.</p>
+
+    </div>
+  )
+}
+
+const Player = () => {
+  return (
+    <div>
+      <label>Player name:</label>
+      <input type="text"></input>
+      <br></br>
+
+      <label>Choose a color:</label>
+
+      <select name="color" id="color">
+        <option value="Red">Red</option>
+        <option value="Black">Black</option>
+      </select>
+      <Router>
+        <nav>
+          <Link to="/game">Game</Link>
+        </nav>
+        <Route path="/game" component={Game}></Route>
+      </Router>
+    </div>
+  )
+}
+
+class Game extends React.Component {
+  render() {
+
+    const cells = [];
+
+    for (let y = 0; y < 7; y++) {
+      const row = [];
+      for (let x = 0; x < 8; x++) {
+        row.push(<Cell key={x} x={x} y={y} />);
+      }
+      cells.push(<div className="row">{row}</div>)
+    }
+
+    return (
+      <div className="cell">
+        {cells}
+      </div>
+    )
+  }
+
+}
+
+class Cell extends React.Component {
+  render() {
+    return (
+      <div className="grid">
+        <p>{this.props.x},{this.props.y}</p>
+      </div>
+    )
+  }
+
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+        </ul>
+        <ul>
+          <Link to="/player">Player</Link>
+        </ul>
+          
+      </nav>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/player" component={Player}></Route>
+
+      </div>
+    </Router>
+
   );
 }
 
